3c1f98433e8e0bc315ed6e16d0781538d0f2b0e8
LucianoGostos4o <118531665+LucianoGostos4o@users.noreply.github.com>
2024-09-19 09:08:51 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let j = 0; j < botoes.length; j++){
+    botoes[j].onclick = function(){
+        
+    }
+
+}
