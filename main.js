f9274e3b5fedfc811f6eee8cdde6520ebd370e97
LucianoGostos4o <118531665+LucianoGostos4o@users.noreply.github.com>
2024-10-10 08:39:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,33 @@ for(let j = 0; j < botoes.length; j++){
     botoes[j].onclick = function(){
         
     }
+    const botões = document. querySelectorAll(".botao");
+const textos = document. querySelectorAll(".aba-conteudo");
+
+for(let j = 0; j < botoes.length; j++){
+    botoes[j].onclick = function(){
+        for(let i = 0; i < botoes.length; i++){
+            botoes[i].classList.remove("ativo");
+            botoes[i].classList.remove("ativo");
+        }
+    }
+   
+    botoes[i].classList.add("ativo");
+    botoes[i].classList.add("ativo");
+
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2024-10-01T00:00:00");
+const tempoObjetivo2 = new Date ("2025-12-25T00:00:00");
+const tempoObjetivo3 = new Date ("2026-06-26T00:00:00");
+const tempoObjetivo4 = new Date ("2027-05-24T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function calcularTempo(tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetvio - tempoAtual;
+}
 
 }
